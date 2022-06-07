7f4b3fb9b5f41357150ccf161a7c9c2ef005a93b
wan-siang <80023755+wan-siang@users.noreply.github.com>
2022-06-07 23:19:53 +0800

    Update 基礎15.cpp

--- itsa/基礎15.cpp
+++ itsa/基礎15.cpp
@@ -5,22 +5,23 @@ using namespace std;
 
 int main(){
     string str;
-    int arr[26];
+    int arr[26];    //idx:0~25 -> alphabet: a~z, 存字母出現個數
     int pos;
-    int word_cnt=0;
+    int word_cnt = 0;
     char c;
-
-    for(int i=0;i<26;i++){
-        arr[i]=0;
+    
+    //初始化
+    for(int i = 0; i < 26; i++){
+        arr[i] = 0;
     }
 
     getline(cin, str);
     int len = str.length();
 
-    for(int i=0;i<len;i++){
-        if(str[i]==' '){
-            word_cnt++;
-        }else if('A'<=str[i] && str[i]<='Z'){
+    for(int i = 0; i < len; i++){
+        if(str[i] == ' '){
+            word_cnt++; //字數=空格數+1
+        }else if('A' <= str[i] && str[i] <= 'Z'){
             pos = str[i] - 'A';
             arr[pos]++;
         }else{
@@ -30,7 +31,8 @@ int main(){
     }
 
     cout<<word_cnt+1<<endl;
-    for(int i=0;i<26;i++){
+    //輸出字母出現次數
+    for(int i = 0; i < 26; i++){
         if(arr[i]){
             c = 'a' + i;
             cout<<c<<" : "<<arr[i]<<endl;
