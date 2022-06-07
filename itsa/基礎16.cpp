c5ad97c00b2406eeba0ca7c878b58fb8ebe23577
wan-siang <80023755+wan-siang@users.noreply.github.com>
2022-06-07 23:23:28 +0800

    Update 基礎16.cpp

--- itsa/基礎16.cpp
+++ itsa/基礎16.cpp
@@ -7,12 +7,12 @@ int main(){
     int pos = 0;
     int cnt = 0;
 
-    cin>>subStr;
-    cin>>str;
-
+    cin >> subStr;  
+    cin >> str;
+    //找出subStr在str出現的次數
     while(1){
         pos = str.find(subStr, pos);
-        if(pos!=string::npos){
+        if(pos != string::npos){    //如果還沒讀到結束位置
             pos++;
             cnt++;
         }else{
